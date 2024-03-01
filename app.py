0440a9f2517162e2a9da5315f4a31458872f5357
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-03-01 11:39:45 -0600

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ minutes_df = pd.DataFrame({'Minute': [str(i).zfill(2) for i in range(60)]})
 hora = st.selectbox("Selecciona la hora", hours_df)
 minuto = st.selectbox("Selecciona el minuto", minutes_df)
 
-target_time_low = datetime.strptime('2024-02-29 ' + hora + ':' + minuto + '00.000000', '%Y-%m-%d %H:%M:%S.%f')
+target_time_low = datetime.strptime('2024-02-29 ' + hora + ':' + minuto + ':' + '00.000000', '%Y-%m-%d %H:%M:%S.%f')
 
 st.write(target_time_low)
 # target_time_high = datetime.strptime('2024-02-29 18:34:00.006000', '%Y-%m-%d %H:%M:%S.%f')
