a93e70cee5119917ca809359d3a38d8e577e364b
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-03-04 13:37:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -154,14 +154,14 @@ if codigo == st.secrets["codigo_familiar"]:
     orders = pd.DataFrame(orders_data)
     orders[['price', 'size', 'notional', 'fee']] = orders[['price', 'size', 'notional', 'fee']].apply(pd.to_numeric)
     symbol_for_sell = symbol_for_sell + '_USDT'
-    st.dataframe(orders)
+    # st.dataframe(orders)
     orders = orders[(orders['symbol']==symbol_for_sell) & (orders['side']=='buy')]
-    st.dataframe(orders)
-    # orders['Total Price'] = orders['notional'] + orders['fee']
-    # orders = orders.groupby(by=['symbol'], as_index=False).agg({'size': 'sum', 'Total Price': 'sum'})
-    # orders['Precio Prom Compra'] = orders['Total Price'] / orders['size']
-    # st.dataframe(orders)    
-# Precio_promedio = orders['Precio Prom Compra'].values[0]
+    # st.dataframe(orders)
+    orders['Total Price'] = orders['notional'] + orders['fee']
+    orders = orders.groupby(by=['symbol'], as_index=False).agg({'size': 'sum', 'Total Price': 'sum'})
+    orders['Precio Prom Compra'] = orders['Total Price'] / orders['size']
+    st.dataframe(orders)    
+    Precio_promedio = orders['Precio Prom Compra'].values[0]
       #-----
 
 
