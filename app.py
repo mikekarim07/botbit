9d831f88c9b15a6f5b96a94fed804dc8ee650fcb
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-03-07 00:12:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -153,7 +153,7 @@ if codigo == st.secrets["codigo_familiar"]:
     st.divider()
 
     st.subheader('Compra Manual en caso de que no jale la compra automatica')
-    monto_manual = st.text_input('Ingresa el monto en USDT a comprar, por default son 50', '50')
+    monto_manual = st.text_input('Ingresa el monto en USDT a comprar')
     st.write('Vas a comprar ' + monto_manual + ' de: ' + symbol_for_bot)
     if st.button('Comprar Manual'):
       orden_compra(symbol_for_bot, monto_manual)
