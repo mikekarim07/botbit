f5081e083d99c6421a82ea08c1e8c73897183fbb
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-03-14 11:55:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -232,8 +232,7 @@ if codigo == st.secrets["codigo_familiar"]:
     wallet_for_screen = wallet_for_screen.merge(orders, left_on="id", right_on='symbol', how='left')
     wallet_for_screen = wallet_for_screen[['id','total', 'Precio Prom']]
     wallet_for_screen = wallet_for_screen.fillna(0)
-    wallet_for_screen['Precio Prom'] = wallet_for_screen['Precio Prom'].apply(lambda x: pd.to_numeric(x, errors='coerce'))
-    wallet_for_screen['Precio Prom'] = wallet_for_screen['Precio Prom'].round(15)
+    wallet_for_screen[['Precio Prom']].apply(decimal)
     # symbols = 
     # for symbol in df['symbol']:
     #     symbol_info = spotapi.get_symbol_ticker(symbol)
